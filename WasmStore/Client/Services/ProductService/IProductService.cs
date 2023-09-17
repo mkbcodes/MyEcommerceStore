50cd7d890b231234d3d68568daa11de21176c0ec
mkbcodes <mkbcodes@gmail.com>
2023-09-16 20:44:50 -0400

    Setup product contract interface and implemented a product service class.

---
+++ WasmStore/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using WasmStore.Shared;
+
+namespace WasmStore.Client.Services.ProductService
+{
+    /// <summary>
+    /// Responsible for interacting with the user interface, managing client-side state, and making HTTP calls to the server.
+    /// </summary>
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Product Product { get; set; }
+        Task GetProducts();
+        Task<ServiceResponse<Product>> GetProduct(int productId);
+        Task PostProduct(Product product);
+    }
+}
---
+++ WasmStore/Server/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WasmStore.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // Asynchronous call to get a list of products, basically IActionResult defines a contract that is the result of a request.
+        // When the client (in my case, the Blazor WASM app) sends a GET request to the server to ask for products, this method will be executed.The IActionResult is a way for the server to package up its response to the client's HTTP request. and The Ok(Products) part essentially means that the server is responding with a "200 OK" status code along with the data (Products in this example). This is a standard way to say "Hey, everything went well, and here's the data you asked for."
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
+        {
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> CreateProduct(Product product)
+        {
+            var result = await _productService.PostProductAsync(product);
+            return Ok(result);
+        }
+    }
+}
